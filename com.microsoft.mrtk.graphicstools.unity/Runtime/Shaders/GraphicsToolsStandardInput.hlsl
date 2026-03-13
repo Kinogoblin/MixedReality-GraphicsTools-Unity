7d7bbe7ff56d907912507a4be1647d997c31a7f6
Kinogoblin <kinogoblin@gmail.com>
2026-03-13 19:38:20 +0100

    Update GraphicsToolsStandardInput.hlsl

--- com.microsoft.mrtk.graphicstools.unity/Runtime/Shaders/GraphicsToolsStandardInput.hlsl
+++ com.microsoft.mrtk.graphicstools.unity/Runtime/Shaders/GraphicsToolsStandardInput.hlsl
@@ -82,9 +82,6 @@ struct Varyings
     half3 worldNormal : COLOR3;
 #endif
 #endif
-#if defined(_RECEIVESHADOW) && defined(_URP)
-    half4 shadowCoord : TEXCOORD4;
-#endif
     UNITY_VERTEX_INPUT_INSTANCE_ID
     UNITY_VERTEX_OUTPUT_STEREO
 };
